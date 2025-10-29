cbf68baf59864cff271cc769a8f9c447fd159f68
pedrohmadruga <pedrohfonseca01@gmail.com>
2025-10-29 11:11:25 -0300

    feat :arrow_up: Bricks collision, better collision with plaftform and border

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const BRICK_MIN_WIDTH = 60;
 const BRICK_MAX_WIDTH = 100;
 const ROWS = 8;
 
+let ballIsMoving;
 
 class Ball {
     #xPosition;
@@ -38,6 +39,8 @@ class Ball {
 
     set xSpeed(xSpe) { this.#xSpeed = xSpe };
     set ySpeed(ySpe) { this.#ySpeed = ySpe };
+    set x(x) { this.#xPosition = x};
+    set y(y) { this.#yPosition = y};
 
 
     set radius(radius) { this.#radius = radius }
@@ -118,7 +121,9 @@ class Brick {
 
     draw = function(ctx) {
         ctx.fillStyle = "red";
-        ctx.fillRect(this.#xPosition, this.#yPosition, this.#width, this.#height);
+        if (this.isVisible) {
+            ctx.fillRect(this.#xPosition, this.#yPosition, this.#width, this.#height);
+        }
     }
 }
 
@@ -170,8 +175,12 @@ function generateBricks() {
     return bricks;
 }
 
+// "Sticks" a value inside an interval
+function clamp(value, min, max) {
+    return Math.max(min, Math.min(max, value));
+}
 
-let ballIsMoving = false;
+resetGame();
 
 canvas.addEventListener("mousedown", () => {
     if (!ballIsMoving) {
@@ -197,21 +206,91 @@ function gameLoop() {
 
     if (ballIsMoving) {
         ball.update();
-        if (ball.x >= canvas.width || ball.x <= 0) { ball.reverseX() } // Hit right or left border
-        if (ball.y <= 0) { ball.reverseY() } // Hit top border
-        if (ball.y >= platform.y && ball.x >= platform.x && ball.x <= platform.x + platform.width) { // Hit the platform
-            if (ball.x >= platform.x && ball.x < platform.x + platform.width / 2) { // Hit left side of the platform
-                ball.xSpeed = Math.abs(ball.xSpeed) * -1;
-            }
-            else if (ball.x <= platform.x + platform.width && ball.x > platform.x - platform.width / 2) { // Hit right part of the platform
-                ball.xSpeed = Math.abs(ball.xSpeed);
-            }
+
+        // Check collisions
+        // Lateral collisions
+        if (ball.x - ball.radius <= 0) { // Left
+            ball.x = ball.radius; 
+            ball.reverseX();
+        } 
+        else if (ball.x + ball.radius >= canvas.width) { // Right
+            ball.x = canvas.width - ball.radius;
+            ball.reverseX();
+        }
+
+        // Top collision
+        if (ball.y - ball.radius <= 0) {
+            ball.y = ball.radius;
             ball.reverseY();
         }
-        else if (ball.y >= canvas.height) { // Hit the bottom border
+
+        // Platform collision
+        const closestX = clamp(ball.x, platform.x, platform.x + platform.width);
+        const closestY = clamp(ball.y, platform.y, platform.y + platform.height);
+
+        const deltaX = ball.x - closestX;
+        const deltaY = ball.y - closestY;
+        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY)
+
+        if (distance < ball.radius) {
+            // Fix overlaping
+            const overlap = ball.radius - distance;
+            const nx = deltaX / distance; // Unit vectors
+            const ny = deltaY / distance;
+            ball.x += nx * overlap;
+            ball.y += ny * overlap;
+
+            ball.reverseY()
+
+            // Adjusts xSpeed based on the angle it collided with the platform
+            const hitPoint = (ball.x - platform.x) / platform.width - 0.5; 
+            ball.xSpeed = hitPoint * 8; 
+        }
+        
+        else if (ball.y + ball.radius > canvas.height) { 
             console.log('perdeu');
             ballIsMoving = false;
             return;
+        }
+
+        for(const brick of bricks) { // Brick collision
+            if (!brick.isVisible) continue;
+
+            // Closest point from the center of the ball to the rect of the brick
+            const closestX = clamp(ball.x, brick.x, brick.x + brick.width);
+            const closestY = clamp(ball.y, brick.y, brick.y + brick.height);
+
+            const deltaX = ball.x - closestX;
+            const deltaY = ball.y - closestY;
+
+            if(deltaX * deltaX + deltaY * deltaY <= ball.radius * ball.radius) {
+                const overlapX = ball.radius - Math.abs(deltaX);
+                const overlapY = ball.radius - Math.abs(deltaY);
+
+                if (overlapX < overlapY) { // Lateral collision
+                    if (deltaX > 0) { // Collided with the right side of the brick
+                        ball.x += overlapX;
+                    }
+                    else { // Collided with the left side of the brick
+                        ball.x -= overlapX;
+                    }
+                    ball.reverseX();
+                }
+                else { // Vertical collision
+                    if (deltaY > 0) { // Collided with the bot side of the brick
+                        ball.y += overlapY;
+                    }
+                    else { // Collided with the top side of the brick
+                        ball.y -= overlapY;
+                    }
+                    ball.reverseY();
+                }
+                brick.isVisible = false;
+
+                Math.abs(ball.ySpeed) <= 8 && ball.ySpeed > 0 ? (ball.ySpeed += 0.25) : (ball.ySpeed -= 0.25);
+                console.log(ball.ySpeed)
+                break;
+            }
         }
     }
 
@@ -222,6 +301,7 @@ function resetGame() {
     ball.update(canvas.width / 2, platform.y - ball.radius);
     ball.xSpeed = BALL_STARTER_SPEED;
     ball.ySpeed = -BALL_STARTER_SPEED;
+    ballIsMoving = false;
 }
 
 gameLoop();
